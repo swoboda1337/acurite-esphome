4df12800b5bd639a8e9144e4b6e03390d150a484
Jonathan Swoboda <jonathan.swoboda>
2024-04-09 00:03:08 -0400

    Add ook floor setting to sx127x, needed for remote receiver to limit noise

--- components/sx127x/sx127x.cpp
+++ components/sx127x/sx127x.cpp
@@ -12,6 +12,7 @@ const uint8_t REG_FRF_MID         = 0x07;
 const uint8_t REG_FRF_LSB         = 0x08;
 const uint8_t REG_RX_BW           = 0x12;
 const uint8_t REG_OOK_PEAK        = 0x14;
+const uint8_t REG_OOK_FIX         = 0x15;
 const uint8_t REG_SYNC_CONFIG     = 0x27;
 const uint8_t REG_PACKET_CONFIG_1 = 0x30;
 const uint8_t REG_PACKET_CONFIG_2 = 0x31;
@@ -43,10 +44,10 @@ uint8_t SX127x::single_transfer_(uint8_t address, uint8_t value)
 {
   uint8_t response;
   delegate_->begin_transaction();
-  nss_->digital_write(false);
+  nss_pin_->digital_write(false);
   delegate_->transfer(address);
   response = delegate_->transfer(value);
-  nss_->digital_write(true);
+  nss_pin_->digital_write(true);
   delegate_->end_transaction();
   return response;
 }
@@ -55,14 +56,14 @@ void SX127x::setup() {
   ESP_LOGD(TAG, "SX127x setup");
 
   // setup nss and set high
-  nss_->setup();
-  nss_->digital_write(true);
+  nss_pin_->setup();
+  nss_pin_->digital_write(true);
 
   // setup reset and toggle to reset chip
-  rst_->setup();
-  rst_->digital_write(false);
+  rst_pin_->setup();
+  rst_pin_->digital_write(false);
   delay(1);
-  rst_->digital_write(true);
+  rst_pin_->digital_write(true);
   delay(10);
 
   // start spi
@@ -97,6 +98,9 @@ void SX127x::setup() {
   write_register_(REG_SYNC_CONFIG, 0x00);
   write_register_(REG_OOK_PEAK, OOK_TRESHOLD_PEAK);
 
+  // set ook floor
+  write_register_(REG_OOK_FIX, int((128 + this->ook_floor_) * 2 + 0.5));
+
   // enable rx mode  
   write_register_(REG_OP_MODE, modulation_ | MODE_LF_ON | MODE_STDBY);
   delay(1);
@@ -113,6 +117,9 @@ void SX127x::dump_config() {
   ESP_LOGCONFIG(TAG, "  Frequency: %f MHz", (float)this->frequency_ / 1000000);
   ESP_LOGCONFIG(TAG, "  Bandwidth: %.1f kHz", (float)rx_bw / 1000);
   ESP_LOGCONFIG(TAG, "  Modulation: %s", this->modulation_ == MODULATION_FSK ? "FSK" : "OOK");
+  if (this->modulation_ == MODULATION_OOK) {
+    ESP_LOGCONFIG(TAG, "  Floor: %.1f dBm", this->ook_floor_);
+  }
   if (this->is_failed()) {
     ESP_LOGE(TAG, "Configuring SX127x failed");
   }
